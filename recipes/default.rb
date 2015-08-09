78498d61f63a529cd2b9012901514fff89840b8f
nfernal <nfernal13@outlook.com>
2015-08-09 16:19:20 -0500

    updated .kitchen.yml for centos vagrant box and added default.rb in attributes folder

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,15 @@
 # Cookbook Name:: lamp
 # Recipe:: default
 #
-# Copyright (c) 2015 The Authors, All Rights Reserved.
+# Copyright (c) 2015 The Authors, All Rights Reserved
+	
+execute "sudo yum -y update" do
+	action :run
+end	
+
+%w{ncompress sharutils}.each do |pkg|
+	package pkg do
+		action :install
+	end
+end
+
